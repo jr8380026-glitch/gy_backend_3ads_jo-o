c92f9f855869fea21d14f158a4a16be03445f13d
jr8380026-glitch <jr8380026@gmail.com>
2026-03-05 14:16:23 +0000

    finalizar if-else e introdução aos arrays

---
+++ arrays/arrays-time.js
@@ -0,0 +1,12 @@
+// um array é um grupo inexado de valores
+// podem ser homogéneos / heterogéneos
+// seus indices vão de "0"..."infinito"
+//possuem função nativas (prontas)
+// podem ser interados ou checados
+
+const time= ['peter', 'maria', 'mark']
+   // indices  0         1        2
+
+   console.log(time)
+   console.log(time[32])
+   console.log(time[1]) 
